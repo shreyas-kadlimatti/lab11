1b542bfa7bfa96dc15e47af67ed2874731f6891f
shreyas-kadlimatti <shreyaskadlimatti9@gmail.com>
2026-06-19 14:14:40 +0530

    commit

---
+++ src/main/java/org/example/app.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class app {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
+    }
+}
